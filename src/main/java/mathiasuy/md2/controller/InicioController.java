e63d1ab31203ffbf5a48880a4014031ed058de50
Mathias Battistella <mathias2b@gmail.com>
2020-07-02 00:15:31 -0300

    buttons

--- src/main/java/mathiasuy/md2/controller/InicioController.java
+++ src/main/java/mathiasuy/md2/controller/InicioController.java
@@ -6,10 +6,27 @@ import java.util.ResourceBundle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class InicioController implements Initializable  {
 
+    @FXML TextField txtResConjunto;
+    @FXML TextArea txtConsola;
+    @FXML TextField txtGeneradores;
+    @FXML TextField txtPares;
+    @FXML TextField txtResMcd;
+    @FXML TextField txtResMcm;
+    @FXML CheckBox chkBox;
+    @FXML TextField txtN;
+    @FXML TextField txtResPhi;
+    @FXML TextField txtResCantidadGeneradores;
+    @FXML ListView<String> lstOrden;
+	
 	private static Logger logger = LogManager.getLogger(InicioController.class);
 
 	
